1b8e38cb6246d6d3beee7215df1852021918c7e4
JKajas <jakub.kajas@gmail.com>
2024-04-15 16:20:14 +0200

    Add UART basic implementation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![no_main]
 #![no_std]
+
+use crate::synchronization::interface::Mutex;
 mod bsp;
 mod console;
 mod cpu;
@@ -8,6 +10,17 @@ mod print;
 mod synchronization;
 
 pub fn kernel_init() -> ! {
-    println!("Hello WORLD!");
+    use bsp::bcm::bcm2711_uart::Uart;
+    let uart = unsafe { Uart::new(0xFE20_1000) };
+    unsafe {
+        uart.inner.lock(|inner| {
+            inner.init(
+                bsp::bcm::ParityBit::None,
+                bsp::bcm::WordLength::Bit8,
+                bsp::bcm::StopBits::One,
+                115200,
+            )
+        })
+    };
     panic!("Stopping here");
 }
